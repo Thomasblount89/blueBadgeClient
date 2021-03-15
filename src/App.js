89b6b90dfd7ad447996076ed6db902157161a944
Tom <tomrambo95@yahoo.com>
2021-03-15 13:51:46 -0400

    added header file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import {useState, useEffect} from 'react';
 import Movie from './components/Movie'
+import Header from './components/Header/Header'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const featured_API = `https://api.themoviedb.org/3/movie/popular?api_key=58269892c382f28ba4692e1cab597755&language=en-US&page=1`
 const search_API = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=`
@@ -20,9 +22,12 @@ function App() {
 
   return (
     <div>
+      <Router>
         <header >
+          <Header />
           <input className='search' type='text' placeholder='Search...'></input>
         </header>
+      </Router>
 
         <div className='movie-container'>
         {movies.length > 0 && movies.map(movie => (
