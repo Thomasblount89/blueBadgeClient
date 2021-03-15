955b708808034620ab077c4b212a91011a60e4ce
Derek <derek101225@gmail.com>
2021-03-15 12:44:13 -0400

    base layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import {useState} from 'react';
-import Auth from './components/Auth/Auth';
-// import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import Movie from './components/Movie'
+
+const featured_API = `https://api.themoviedb.org/3/movie/popular?api_key=58269892c382f28ba4692e1cab597755&language=en-US&page=1`
+const search_API = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=`
+
 
 
 function App() {
-const [sessionToken, setSessionToken] = useState(undefined);
-console.log(sessionToken);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(featured_API)
+      .then(res => res.json())
+      .then(data => { console.log(data);
+        setMovies(data.results)});
+
+    
+  }, [])
 
   return (
-    <div className="App">
+    <div>
+        <header >
+          <input className='search' type='text' placeholder='Search...'></input>
+        </header>
 
-  
-     <Auth />
+        <div className='movie-container'>
+        {movies.length > 0 && movies.map(movie => (
+          <Movie key={movie.id} {...movie}/>
+        ))}
+        </div>
+    
     </div>
   );
+
 }
 
 export default App;
