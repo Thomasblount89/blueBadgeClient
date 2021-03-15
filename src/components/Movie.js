05da73985d1cbb8e50c23d675120298408e0d91e
Tom <tomrambo95@yahoo.com>
2021-03-15 17:22:19 -0400

    before pulling from derek

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import {Link} from 'react'
 
 const IMG_API = `https://image.tmdb.org/t/p/w1280`
 
 const search_API = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=`
 
-const Movie = ({title, poster_path, overview, vote_average}) => {
+const Movie = ({title, poster_path, overview, vote_average, id}) => {
 
 
     return(
-
+        // <Link to='/SingleMoive' movieId={id}>
         <div className='movie'>
             <img src={IMG_API + poster_path} alt={title} />
             <div className='movie-info'>
@@ -21,8 +21,7 @@ const Movie = ({title, poster_path, overview, vote_average}) => {
                 <p>{overview}</p>
             </div>
         </div> 
-        
-    
+        // </Link>
     )
 }
 
