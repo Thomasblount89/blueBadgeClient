61dc03ae80d3039a591929e020b6f8879e267654
Derek <derek101225@gmail.com>
2021-03-15 17:20:15 -0400

    updated componets

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import MovieFlix from './components/MovieFlix'
+import { BrowserRouter, Route, Link, Switch, Router } from "react-router-dom";
 
 const search_API = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=`
 
@@ -6,11 +7,28 @@ const search_API = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a8
 function App() {
   return (
     <div>
+    <Router>
         <header >
+        <Link to='/'>MovieFlix</Link>
           <input className='search' type='text' placeholder='Search...'></input>
+
         </header>
 
-    <MovieFlix />
+    </Router>
+
+    <BrowserRouter>
+        <Switch>
+      
+          <Route path='/' exact render={() => <MovieFlix />} />
+          
+          
+          
+                
+        </Switch>
+      </BrowserRouter>
+
+
+
     </div>
   );
 
