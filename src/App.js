43aca55831cb0144b1ce6ea4dbe52e89c3bd7894
Thomas Blount <Thomasblount89@gmail.com>
2021-03-15 17:28:13 -0400

    update 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-
+import {useState, useEffect} from 'react';
 import Movie from './components/Movie'
 import Header from './components/Header/Header'
 import { BrowserRouter as Router } from 'react-router-dom';
+import Auth from './components/Auth/Auth';
+import { Navbar } from 'react-bootstrap';
+
 
 const featured_API = `https://api.themoviedb.org/3/movie/popular?api_key=58269892c382f28ba4692e1cab597755&language=en-US&page=1`
 const search_API = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=`
@@ -52,7 +55,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <Navbar clearLocalStorage={clearLocalStorage} />
+         <Navbar clearLocalStorage={clearLocalStorage} />
         {viewConductor()}
       </div>
 
