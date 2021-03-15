99540f266a0de9fb0dfb6ccb3487ff57f8e2e438
Derek <derek101225@gmail.com>
2021-03-15 15:54:39 -0400

    updated componets

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
-import {useState, useEffect} from 'react';
-import Movie from './components/Movie'
+import MovieFlix from './components/MovieFlix'
 
-const featured_API = `https://api.themoviedb.org/3/movie/popular?api_key=58269892c382f28ba4692e1cab597755&language=en-US&page=1`
 const search_API = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=`
 
 
-
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    fetch(featured_API)
-      .then(res => res.json())
-      .then(data => { console.log(data);
-        setMovies(data.results)});
-
-    
-  }, [])
-
   return (
     <div>
         <header >
           <input className='search' type='text' placeholder='Search...'></input>
         </header>
 
-        <div className='movie-container'>
-        {movies.length > 0 && movies.map(movie => (
-          <Movie key={movie.id} {...movie}/>
-        ))}
-        </div>
-    
+    <MovieFlix />
     </div>
   );
 
 }
-
 export default App;
---
+++ src/components/SingleMovie.js
@@ -0,0 +1,17 @@
+
+const IMG_API = `https://image.tmdb.org/t/p/w1280`
+
+
+const SingleMovie = ({title, poster_path, overview, vote_average, id}) => {
+
+
+    return(
+        <div>
+            
+        </div>
+        
+    )
+}
+
+export default SingleMovie;
+
