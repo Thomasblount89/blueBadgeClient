3d7330596bca98dc1fc99f36f236e749316a5914
Derek <derek101225@gmail.com>
2021-03-16 17:43:04 -0400

    updated flow of componets

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
-import MovieFlix from './components/MovieFlix'
-import { BrowserRouter, Route, Link, Switch, Router } from "react-router-dom";
-
-const search_API = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=`
+import {useState} from 'react';
+import Auth from './components/Auth/Auth';
+import MovieFlix from './components/MovieFlix/MovieFlix';
 
 
 function App() {
+const [sessionToken, setSessionToken] = useState(undefined);
+console.log(sessionToken);
+
   return (
-    <div>
-    <Router>
-        <header >
-        <Link to='/'>MovieFlix</Link>
-          <input className='search' type='text' placeholder='Search...'></input>
-
-        </header>
-
-    </Router>
-
-    <BrowserRouter>
-        <Switch>
-      
-          <Route path='/' exact render={() => <MovieFlix />} />
-          
-          
-          
-                
-        </Switch>
-      </BrowserRouter>
-
-
-
+    <div className="App">
+      <MovieFlix />
     </div>
   );
-
 }
+
 export default App;
