d28c8781a1c1ea71a1090703d27978692dccf21d
Derek <derek101225@gmail.com>
2021-03-17 02:18:38 -0400

    fixed single view

--- src/components/MovieFlix/SingleMovie/SingleMovie.js
+++ src/components/MovieFlix/SingleMovie/SingleMovie.js
@@ -3,17 +3,20 @@ const IMG_API = `https://image.tmdb.org/t/p/w1280`;
 const SingleMovie = props => {
     return(
         <div>
-            <div className='movie' key={props.singleMovie.id}>
+            <div className='single' key={props.singleMovie.id}>
                 <img src={IMG_API + props.singleMovie.poster_path} alt={props.singleMovie.title} />
-                <div className='movie-info'>
-                    <h3>{props.singleMovie.title}</h3>
-                    <span>{props.singleMovie.vote_average}</span>
-                </div>
-                <div className='movie-over'>
-                    <h2>Overview:</h2>
-                    <p>{props.singleMovie.overview}</p>
-                </div>
+                        <div className='movie-info'>
+                            
+                        </div> 
             </div>
+
+                <h3 className='single' >{props.singleMovie.title}</h3>
+                <span className='single'>Rating: {props.singleMovie.vote_average}</span>
+                        <div className='overviewSingle' >
+                            <h2 className='single'>Overview:</h2>
+                            <p className='over'>{props.singleMovie.overview}</p>
+                        </div>
+           
             
         </div>
     )
