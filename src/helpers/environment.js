61657b3881c2626b591b270c813ae7680b94949f
Tom <tomrambo95@yahoo.com>
2021-03-18 17:00:54 -0400

    changed client url

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = "http://localhost:3001";
         break;
-    case 'tlr-my-flix.herokuapp.com':
+    case 'tlr-my-flix-client.herokuapp.com':
         APIURL = 'https://tlr-my-flix.herokuapp.com'
 }
 
